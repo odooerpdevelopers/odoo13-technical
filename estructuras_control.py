f973cff39ea2dea3be2cc72727f4e529fa73d46e
jcarlosmontoya <jcmontoya@praxyaformaplus.com>
2019-12-09 17:32:31 +0100

    agregar ficheros python clase 1

---
+++ estructuras_control.py
@@ -0,0 +1,9 @@
+precio = 150
+if precio > 100:
+    precio = precio - 20
+elif precio >= 150:
+    precio +=50
+else:
+    precio = precio + 20
+
+print(precio)
---
+++ variables.py
@@ -0,0 +1,5 @@
+number = 100
+
+a_list = [1, "Computer", "Python Class", 5]
+
+print(number, "  ", a_list)
